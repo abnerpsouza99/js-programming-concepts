90b79df41c2b2ec2bd1b546e5119ceac580f8652
Abner Pena <abnertecnicosenac@gmail.com>
2023-04-20 23:52:08 -0300

    Adds file for shorts videos

---
+++ shorts/index.js
@@ -0,0 +1,14 @@
+// Function
+const sumFunction = function (a, b) {
+    return a + b;
+}
+// Arrow Function
+const sumArrowFunction = (a, b) => a + b;
+
+
+console.log(sumFunction(1, 1))
+console.log(sumArrowFunction(1, 1))
+
+
+
+
