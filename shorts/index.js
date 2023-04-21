a77368ee32e150524985e2f069a89ac77f9b6458
Abner Pena <abnertecnicosenac@gmail.com>
2023-04-21 10:54:24 -0300

    Adds code for new shorts related to ways to access property in JS

--- shorts/index.js
+++ shorts/index.js
@@ -1,14 +1,27 @@
-// Function
-const sumFunction = function (a, b) {
-    return a + b;
+// // Function
+// const sumFunction = function (a, b) {
+//     return a + b;
+// }
+// // Arrow Function
+// const sumArrowFunction = (a, b) => a + b;
+
+
+// console.log(sumFunction(1, 1))
+// console.log(sumArrowFunction(1, 1))
+
+
+
+const pessoa = {
+    nome: 'Abner',
+    canalYt: 'Canal - eu sou programador'
 }
-// Arrow Function
-const sumArrowFunction = (a, b) => a + b;
 
+// 1 Notacao de ponto
+console.log(pessoa.canalYt)
 
-console.log(sumFunction(1, 1))
-console.log(sumArrowFunction(1, 1))
+// 2 Notacao de colchetes
+console.log(pessoa['canalYt'])
 
-
-
-
+// 3 Destructuring
+const { canalYt } = pessoa
+console.log(canalYt)
