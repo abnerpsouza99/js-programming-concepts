5f27fab250156158dd648237dc876b64c21ec847
Abner Pena <abnertecnicosenac@gmail.com>
2023-04-24 18:06:30 -0300

    Adiciona operadores de comparacao

--- shorts/index.js
+++ shorts/index.js
@@ -2,20 +2,32 @@
 const numero = 11;
 let mensagem = '';
 
-// If convencional
-if (numero > 10) {
-    mensagem = `Número é maior que 10`;
-} else {
-    mensagem = `Número não é maior que 10`;
-}
+// // If convencional
+// if (numero > 10) {
+//     mensagem = `Número é maior que 10`;
+// } else {
+//     mensagem = `Número não é maior que 10`;
+// }
 
-console.log(mensagem);
+// console.log(mensagem);
 
-// If ternário
-mensagem = numero > 10 ? `Número é maior que 10`
-    : `Número não é maior que 10`;
+// // If ternário
+// mensagem = numero > 10 ? `Número é maior que 10`
+//     : `Número não é maior que 10`;
 
-console.log(mensagem);
+// console.log(mensagem);
+
+
+// Operadores de comparação
+const a = 5;
+const b = 10;
+
+console.log(a == b); // Igual a 
+console.log(a != b); // Diferente de 
+console.log(a > b); // Maior que 
+console.log(a < b); // Menor que 
+console.log(a >= b); // Maior ou igual a 
+console.log(a <= b); // Menor ou igual a 
 
 
 
