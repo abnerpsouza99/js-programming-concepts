4f611b72d90c687262c860c7e2bf83b2eda7ef3b
Abner Pena <abnertecnicosenac@gmail.com>
2023-04-29 11:02:28 -0300

    Adicionando novos shorts

---
+++ shorts/index-2.js
@@ -0,0 +1,15 @@
+const person = {
+    name: 'Abner',
+    ytChannel: 'Canal - Eu Sou Programador'
+}
+
+// Adicionando propriedade ao objeto
+person.age = 23;
+
+console.log(person)
+
+// Removendo propriedade do objeto
+delete person.age;
+
+console.log(person)
+
---
+++ shorts/index-3.js
@@ -0,0 +1,17 @@
+const person = {
+    name: 'Abner',
+    ytChannel: 'Canal - Eu Sou Programador',
+    age: 23,
+    subscribers: 102
+};
+
+// Capturando chaves de um objeto
+const keys = Object.keys(person);
+
+console.log(keys);
+
+// Capturando valores de um objeto
+const values = Object.values(person);
+
+console.log(values);
+
